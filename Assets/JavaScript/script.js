c1fadb782cb9c1fcd13879b92e7e391670dc3087
Sealcakes <taylorcseal@gmail.com>
2023-01-09 20:49:46 -0600

    adds API call

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -18,18 +18,18 @@ if (ingredientList) {
     
 }
 
+// API CALL
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '46bd054f8cmsh16bbe6b909a599dp11e469jsnb89084f34eb0',
+		'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+	}
+};
 
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '46bd054f8cmsh16bbe6b909a599dp11e469jsnb89084f34eb0',
-// 		'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
-// 	}
-// };
 
 
-
-// fetch('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=onion%2Ccelery%2Cchicken&number=5&ignorePantry=true&ranking=1', options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
+fetch('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=onion%2Ccelery%2Cchicken&number=5&ignorePantry=true&ranking=1', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
