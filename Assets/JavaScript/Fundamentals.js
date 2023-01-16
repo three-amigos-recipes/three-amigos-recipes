6f84df87ddebd8d900db5fe67403f8e7e8141673
Christopher Baldock <christophermbaldock@gmail.com>
2023-01-16 12:45:17 -0600

    displays a video if a side button is clicked

--- Assets/JavaScript/Fundamentals.js
+++ Assets/JavaScript/Fundamentals.js
@@ -10,20 +10,65 @@ $(document).ready(function () {
     });
 });
 
+var utensil = document.querySelector('.utensil');
+var utensilVideo = document.querySelector('.utensilVideo');
+var etiquette = document.querySelector('.etiquette');
+var etiquetteVideo = document.querySelector('.etiquetteVideo');
+var beef = document.querySelector('.beef');
+var beefVideo = document.querySelector('.beefVideo');
+var chicken = document.querySelector('.chicken');
+var chickenVideo = document.querySelector('.chickenVideo');
+var pork = document.querySelector('.pork');
+var porkVideo = document.querySelector('.porkVideo');
+var veggie = document.querySelector('.veggie');
+var vegVideo = document.querySelector('.vegVideo');
+var keto = document.querySelector('.keto');
+var ketoVideo = document.querySelector('.ketoVideo');
+var atkins = document.querySelector('.atkins');
+var atkinsVideo = document.querySelector('.atkinsVideo');
+var vegetarian = document.querySelector('.vegetarian');
+var vegetarianVideo = document.querySelector('.vegetarianVideo');
+var gluten = document.querySelector('.gluten');
+var glutenVideo = document.querySelector('.glutenVideo');
 
-// Load the IFrame Player API code asynchronously.
-  var tag = document.createElement('script');
-  tag.src = "https://www.youtube.com/player_api";
-  var firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+utensilVideo.style = 'display: none;';
+etiquetteVideo.style = 'display: none;';
+beefVideo.style = 'display: none;';
+chickenVideo.style = 'display: none;';
+porkVideo.style = 'display: none;';
+vegVideo.style = 'display: none;';
+ketoVideo.style = 'display: none;';
+atkinsVideo.style = 'display: none;';
+vegetarianVideo.style = 'display: none;';
+glutenVideo.style = 'display: none;';
 
-  // Replace the 'ytplayer' element with an <iframe> and
-  // YouTube player after the API code downloads.
-  var player;
-  function onYouTubePlayerAPIReady() {
-    player = new YT.Player('ytplayer', {
-      height: '360',
-      width: '640',
-      videoId: 'M7lc1UVf-VE'
-    });
-  }
+utensil.addEventListener('click', function () {
+    utensilVideo.style = 'display: block;'
+});
+etiquette.addEventListener('click', function () {
+    etiquetteVideo.style = 'display: block;'
+});
+beef.addEventListener('click', function () {
+    beefVideo.style = 'display: block;'
+});
+chicken.addEventListener('click', function () {
+    chickenVideo.style = 'display: block;'
+});
+pork.addEventListener('click', function () {
+    porkVideo.style = 'display: block;'
+});
+veggie.addEventListener('click', function () {
+    vegVideo.style = 'display: block;'
+});
+keto.addEventListener('click', function () {
+    ketoVideo.style = 'display: block;'
+});
+vegetarian.addEventListener('click', function () {
+    vegetarianVideo.style = 'display: block;'
+});
+atkins.addEventListener('click', function () {
+    atkinsVideo.style = 'display: block;'
+});
+gluten.addEventListener('click', function () {
+    glutenVideo.style = 'display: block;'
+});
