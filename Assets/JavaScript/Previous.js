da187901d69a9e2971d71db86f244143bfa7081c
Christopher Baldock <christophermbaldock@gmail.com>
2023-01-16 19:41:15 -0600

    adds code

--- Assets/JavaScript/Previous.js
+++ Assets/JavaScript/Previous.js
@@ -12,10 +12,95 @@ $(document).ready(function () {
     });
 });
 
+// let storedRecipes = localStorage.getItem('recipes');
+// let existingRecipes = storedRecipes ? JSON.parse(storedRecipes) : [];
+
+// // Number of recipes to display at a time
+// let numOfRecipes = 5;
+
+// //Starting index of recipes array
+// let startingIndex = 0;
+
+// // Gets the recipe container
+// let recipeContainerEl = document.getElementById('#recipe-container');
+
+// // Get the 'click for more' button
+// let moreBtn = document.getElementById('#more-btn');
+
+// moreBtn.style.display = 'none';
+
+
+// function displayRecipes() {
+//     let displayedRecipes = existingRecipes.slice(startingIndex, startingIndex + numOfRecipes);
+
+//     // Clears the recipe container
+//     recipeContainerEl.innerHTML = '';
+
+//     displayedRecipes.forEach(function (recipe) {
+//         // display the recipe information, for example by creating and appending elements to the DOM.
+//         let recipeEl = document.createElement('a');
+//         recipeEl.setAttribute('href', recipe.sourceUrl);
+
+//         let recipeBox = document.createElement('div');
+//         recipeBox.classList = 'recipe-container';
+
+//         let recipeTitle = document.createElement('h2');
+//         recipeTitle.innerText = recipe.title;
+//         recipeTitle.id = 'title';
+//         recipeTitle.classList = 'card-header-title is-centered';
+
+//         let recipeImg = document.createElement('img');
+//         recipeImg.setAttribute('src', recipe.image);
+//         recipeImg.id = 'picture';
+
+//         recipeBox.append(recipeTitle, recipeImg);
+//         recipeEl.appendChild(recipeBox);
+
+//         recipeContainerEl.appendChild(recipeEl);
+//     });
+
+//     // Checks if there are more recipes to display
+//     if(startingIndex + numOfRecipes >= existingRecipes.length) {
+//         moreBtn.style = 'display: none';
+//     }
+// }
+
+// displayRecipes();
+
+// moreBtn.addEventListener('click', function() {
+//     startingIndex += numOfRecipes;
+//     displayRecipes();
+// });
+
+
+// See about displaying a certain amount and adding a display more option?
+
+
+
 let storedRecipes = localStorage.getItem('recipes');
 let existingRecipes = storedRecipes ? JSON.parse(storedRecipes) : [];
 
-existingRecipes.forEach(function (recipe) {
+// Number of recipes to be displayed at a time
+let numberOfRecipesToDisplay = 10;
+
+// Starting index of the recipes array
+let startingIndex = 0;
+
+// Get the recipe container element
+let recipeContainer = document.querySelector("#recipe-container");
+
+// Get the 'click for more' button
+let moreButton = document.querySelector("#more-button");
+
+function displayRecipes(){
+    // Get the subset of recipes to be displayed
+    let displayedRecipes = existingRecipes.slice(startingIndex, startingIndex + numberOfRecipesToDisplay);
+
+    // Clear the recipe container
+    recipeContainer.innerHTML = "";
+
+    // Loop through the displayed recipes
+    displayedRecipes.forEach(function (recipe) {
     // display the recipe information, for example by creating and appending elements to the DOM.
     let recipeEl = document.createElement('a');
     recipeEl.setAttribute('href', recipe.sourceUrl);
@@ -35,8 +120,17 @@ existingRecipes.forEach(function (recipe) {
     recipeBox.append(recipeTitle, recipeImg);
     recipeEl.appendChild(recipeBox);
 
-    recipeContainerEl.appendChild(recipeEl);
+    recipeContainer.appendChild(recipeEl);
+    });
+    // check if there are more recipes to display
+    if(startingIndex + numberOfRecipesToDisplay >= existingRecipes.length) {
+        moreButton.style.display = "none";
+    }
+}
+
+displayRecipes();
+
+moreButton.addEventListener("click", function(){
+    startingIndex += numberOfRecipesToDisplay;
+    displayRecipes();
 });
-
-
-// See about displaying a certain amount and adding a display more option?
