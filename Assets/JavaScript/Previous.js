d7f038fff584daa368f1994159e185591a3fdb0d
Christopher Baldock <christophermbaldock@gmail.com>
2023-01-17 15:31:29 -0600

    only shows 5 options at a time on the pervious page with a button to show more

--- Assets/JavaScript/Previous.js
+++ Assets/JavaScript/Previous.js
@@ -15,28 +15,55 @@ $(document).ready(function () {
 let storedRecipes = localStorage.getItem('recipes');
 let existingRecipes = storedRecipes ? JSON.parse(storedRecipes) : [];
 
-existingRecipes.forEach(function (recipe) {
-    // display the recipe information, for example by creating and appending elements to the DOM.
-    let recipeEl = document.createElement('a');
-    recipeEl.setAttribute('href', recipe.sourceUrl);
+let currentPage = 0;
 
-    let recipeBox = document.createElement('div');
-    recipeBox.classList = 'recipe-container';
+let numberOfRecipesPerPage = 5;
 
-    let recipeTitle = document.createElement('h2');
-    recipeTitle.innerText = recipe.title;
-    recipeTitle.id = 'title';
-    recipeTitle.classList = 'card-header-title is-centered';
+function displayRecipes() {
+    let recipesToDisplay = existingRecipes.slice(currentPage * numberOfRecipesPerPage, (currentPage + 1) * numberOfRecipesPerPage);
 
-    let recipeImg = document.createElement('img');
-    recipeImg.setAttribute('src', recipe.image);
-    recipeImg.id = 'picture';
+    recipeContainerEl.innerHTML = '';
+    recipesToDisplay.forEach(function (recipe) {
+        // display the recipe information, for example by creating and appending elements to the DOM.
+        let recipeEl = document.createElement('a');
+        recipeEl.setAttribute('href', recipe.sourceUrl);
 
-    recipeBox.append(recipeTitle, recipeImg);
-    recipeEl.appendChild(recipeBox);
+        let recipeBox = document.createElement('div');
+        recipeBox.classList = 'recipe-container';
 
-    recipeContainerEl.appendChild(recipeEl);
+        let recipeTitle = document.createElement('h2');
+        recipeTitle.innerText = recipe.title;
+        recipeTitle.id = 'title';
+        recipeTitle.classList = 'card-header-title is-centered';
+
+        let recipeImg = document.createElement('img');
+        recipeImg.setAttribute('src', recipe.image);
+        recipeImg.id = 'picture';
+
+        recipeBox.append(recipeTitle, recipeImg);
+        recipeEl.appendChild(recipeBox);
+
+        recipeContainerEl.appendChild(recipeEl);
+    });
+    checkButton();
+};
+
+let showMoreBtn = document.querySelector('#showMoreBtn');
+showMoreBtn.addEventListener('click', function() {
+    currentPage++;
+    displayRecipes();
 });
+
+function checkButton() {
+    if(currentPage >= Math.ceil(existingRecipes.length / numberOfRecipesPerPage) - 1) {
+        showMoreBtn.style = 'display: none';
+    } else {
+        showMoreBtn.style = 'display: block';
+    };
+};
+
+displayRecipes();
+
 
 
 
