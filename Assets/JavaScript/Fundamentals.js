dfedfe2b350c970242ee86933c4d480772f28498
Christopher Baldock <christophermbaldock@gmail.com>
2023-01-18 19:17:04 -0600

    deletes unused code

--- Assets/JavaScript/Fundamentals.js
+++ Assets/JavaScript/Fundamentals.js
@@ -227,8 +227,3 @@ gluten.addEventListener('click', function () {
     $('.btn').toggleClass('click');
     $('.sidebar').toggleClass('show');
 });
-
-// $(document).click(function(event) {
-//     $('.btn').toggleClass('click')
-//     $('.sidebar').toggleClass('show');
-// });
